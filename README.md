0ab95f3d2abf4786ad3735e9af41f92e7b58a4c2
Jason Press <99525512+pressjson@users.noreply.github.com>
2024-12-03 22:23:32 -0800

    Updated README

--- README.md
+++
@@ -1,3 +0,0 @@
-# COSC 200
-
-This is Jason Press and Mia Stulac's project for COSC 200
--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+import os
+import subprocess
+
+print("This will run through an example, preparing the Moonlight Sonata in a newly created /data directory.")
+
+url = "https://musescore.com/user/41399859/scores/12043831"
+
+script1 = "./librescore-runner.sh"
+script2 = "./musescore-runner.sh"
+
+subprocess.run([script1, url])
+
+cwd = os.getcwd()
+root = os.path.dirname(cwd)
+data_path = root + "/data"
+
+i = 0
+for infile in
