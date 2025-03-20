db05d66195dc95575f2691c0835901392b30cff0
pressjson <therealcannedtuna@gmail.com>
2025-03-19 23:30:23 -0700

    actually saved changes to stitcher, since i didn't save them (oops). also there was a purge last commit of all the Musescore stuff, and i forgot to mention that

--- src/stitcher.py
+++ src/stitcher.py
@@ -1,1 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+from PIL import Image
+
+
+def concat_horizontal(image_1, image_2) -> Image:
+    combined = Image.new("RGB", (image_1.width + image_2.width, image_1.height))
+    combined.paste(image_1, (0, 0))
+    combined.paste(image_2, (image_1.width, 0))
+    return combined
+
+
+def concat_vertical(image_1, image_2) -> Image:
+    combined = Image.new("RGB", (image_1.width, image_2.width + image_1.height))
+    combined.paste(image_1, (0, 0))
+    combined.paste(image_2, (0, image_1.width))
+    return combined
+
+
+def stitch(directory):
+    """
+    files should be named %name%_%x_offset%_%y_offset%.jpg within the directory
+
     @TODO: write this
+    """
